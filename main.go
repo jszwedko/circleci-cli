19e7adf4f96022c3178ee6eea4e6cece9b09e777
Patrick Debois <Patrick.Debois@jedi.be>
2016-11-21 09:25:46 +0100

    more verbose json errors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -821,6 +822,12 @@ func handleClientError(err error) {
 		default:
 			fmt.Fprintln(os.Stderr, err.Error())
 		}
+	case *json.UnmarshalTypeError:
+		s := fmt.Sprintf("UnmarshalTypeError: Value[%s] Type[%v]\n", err.Value, err.Type)
+		fmt.Fprint(os.Stderr, s)
+	case *json.InvalidUnmarshalError:
+		s := fmt.Sprintf("InvalidUnmarshalError: Type[%v]\n", err.Type)
+		fmt.Fprint(os.Stderr, s)
 	default:
 		fmt.Fprintln(os.Stderr, err.Error())
 	}
